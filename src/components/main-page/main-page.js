32a1640bbce3f62bd8ba7bd6b285743838e30fb0
piotr.chojnacki <chojnacki.piotr91@gmail.com>
2021-06-26 21:23:52 +0200

    added main page

--- src/components/main-page/main-page.js
+++ src/components/main-page/main-page.js
@@ -1,7 +1,19 @@
+import styles from './main-page.module.css';
+import SearchForm from '../_reusable/search-form/search-form';
+
 const MainPage = props => {
   return (
-    <div>
-      Main Page
+    <div className={styles.container}>
+      <h1 className={styles.headerMain}>
+        Welcome to Weatherly.APP!
+      </h1>
+      <h2 className={styles.headerSecondary}>
+        Check the weather in cities around the globe <span role='img' aria-label='emoji-earth'>🌍</span>
+      </h2>
+      <h3 className={styles.headerSecondary}>
+        Type city name below and check actual weather!
+      </h3>
+      <SearchForm/>
     </div>
   );
 };
