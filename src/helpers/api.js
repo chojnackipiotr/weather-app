d888090ff27adfc0e22582b6dd80eff6eed157f8
piotr.chojnacki <chojnacki.piotr91@gmail.com>
2021-06-27 19:34:48 +0200

    update api.js

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,6 +1,6 @@
 export default function fetchCityData(city) {
   return new Promise((resolve, reject) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`)
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=d959d73183b48095c816a6ce3b57da40')
       .then(res => res.json())
       .then(data => resolve(data))
       .catch(err => reject(err))
