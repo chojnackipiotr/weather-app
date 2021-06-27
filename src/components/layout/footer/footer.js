46f128ce4a3167b23734ac8ec5e69dd2b6601dfc
piotr.chojnacki <chojnacki.piotr91@gmail.com>
2021-06-27 09:44:48 +0200

    finished footer

--- src/components/layout/footer/footer.js
+++ src/components/layout/footer/footer.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import styles from './footer.module.css';
 import AppLogo from '../../_reusable/app-logo/app-logo';
 import {FaFacebook, FaInstagram, FaTwitter} from 'react-icons/all';
 import {SiTiktok} from 'react-icons/all';
 
 const Footer = props => {
   return (
-    <footer>
-      <div><AppLogo /> &copy; { new Date().getFullYear() }</div>
-      <div>
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <AppLogo className={styles.logo}/>
+        <span className={styles.copyright}>&copy; {new Date().getFullYear()}</span>
+      </div>
+      <div className={styles.socialMedia}>
         Follow us on:
         <FaFacebook />
         <FaInstagram />
